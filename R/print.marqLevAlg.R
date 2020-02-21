7c02fc11cd33b3ae404a0d2769a908dd09ff5c36
CecileProust-Lima <cecile.proust-lima@inserm.fr>
2020-02-21 11:03:44 +0100

    edits in description and outputs (2)

--- R/print.marqLevAlg.R
+++ R/print.marqLevAlg.R
@@ -2,24 +2,34 @@
 print.marqLevAlg <- function(x,digits=8,...){
 if (!inherits(x, "marqLevAlg")) stop("use only with \"marqLevAlg\" objects")
 
-cl <- x$cl
-cat(" \n")
-dput(cl)
+
 cat(" \n")
 cat("                   Robust marqLevAlg algorithm                   ", "\n")
 cat(" \n")
-cat("Number of parameters:", length(x$b)," \n")
+cl <- x$cl
+dput(cl)
 cat(" \n")
 cat("Iteration process:", "\n")
-
+cat("      Number of parameters:", length(x$b)," \n")
+cat("      Number of iterations:", x$ni, "\n")
+cat("      Optimized objective function:", round(x$fn.value,digits)," \n")
 if(x$istop==1) cat("      Convergence criteria satisfied","\n")
 if(x$istop==2) cat("      Maximum number of iteration reached without convergence","\n")
 if(x$istop==4|x$istop==5)  {
 	cat("      The program stopped abnormally. No results can be displayed.\n")
+}
+cat(" \n")
+cat("Convergence criteria: parameters stability=", round(x$ca[1],digits), "\n")
+cat("                    : objective function stability=", round(x$cb,digits), "\n") 
+if (x$ier == -1){
+	cat("                    : Matrix inversion for RDM failed \n")	
 }else{
+	cat("                    : Matrix inversion for RDM successful \n")
+}
+cat("                    : relative distance to maximum(RDM)=", round(x$rdm,digits), "\n")
+if(x$istop!=4&x$istop!=5) {
 cat(" \n")
-cat("Values:", "\n")
-cat(" \n")
+cat("Final parameter values:", "\n")
 id <- 1:length(x$b)
 indice <- rep(id*(id+1)/2)
 se <-sqrt(x$v[indice])
@@ -31,22 +41,5 @@ bsup <- x$b+1.96*se
 tmp <- data.frame("coef"=format(round(x$b,3)),"SE coef"=format(round(se,3)),"Wald"=format(wald,4),"P-value"=round(1 - pchisq(wald, 1),5),"binf"=round(binf,3),"bsup"=round(bsup,3))
 print(tmp,row.names=F)
 cat(" \n")
-cat("Number of iterations: ", x$ni, "\n")
-cat(" \n")
-cat("Convergence criteria: parameters stability=", round(x$ca[1],digits), "\n")
-cat("                    : objective function stability=", round(x$cb,digits), "\n") 
-if (x$ier == -1){
-	cat("                    : Matrix inversion for RDM failed \n")	
-}else{
-	cat("                    : Matrix inversion for RDM successful \n")
-}
-cat("                    : relative distance to maximum(RDM)=", round(x$rdm,digits), "\n")
-cat(" \n")
-cat("Goodness-of-fit statistics:", "\n")
-cat("      optimized objective function:", round(x$fn.value,digits)," \n")
-cat(" \n")
-cat(" \n")
-cat(" \n")
-
 }
 }
--- R/summary.marqLevAlg.R
+++ R/summary.marqLevAlg.R
@@ -1,33 +1,45 @@
 #' @export
-summary.marqLevAlg <- function(object,digits=8,...)
-{
-    x <- object
+summary.marqLevAlg <- function(x,digits=8,...){
     if (!inherits(x, "marqLevAlg")) stop("use only with \"marqLevAlg\" objects")
-
-    cat(" \n")
-    cat("Values:", "\n")
-    cat(" \n")
-    id <- 1:length(x$b)
-    indice <- rep(id*(id+1)/2)
-    se <-sqrt(x$v[indice])
-    wald <- (x$b/se)**2
-    z <- abs(qnorm((1 + .95)/2))
-    binf <- x$b-1.96*se
-    bsup <- x$b+1.96*se
-    tmp <- data.frame("coef"=format(round(x$b,3)),"SE coef"=format(round(se,3)),"Wald"=format(wald,4),"P-value"=round(1 - pchisq(wald, 1),5),"binf"=round(binf,3),"bsup"=round(bsup,3))
-   # print(tmp,row.names=FALSE)
+    
     
     cat(" \n")
-    cat("Number of iterations: ", x$ni, "\n")
+    cat("                   Robust marqLevAlg algorithm                   ", "\n")
+    cat(" \n")
+    cl <- x$cl
+    dput(cl)
+    cat(" \n")
+    cat("Iteration process:", "\n")
+    cat("      Number of parameters:", length(x$b)," \n")
+    cat("      Number of iterations:", x$ni, "\n")
+    cat("      Optimized objective function:", round(x$fn.value,digits)," \n")
+    if(x$istop==1) cat("      Convergence criteria satisfied","\n")
+    if(x$istop==2) cat("      Maximum number of iteration reached without convergence","\n")
+    if(x$istop==4|x$istop==5)  {
+        cat("      The program stopped abnormally. No results can be displayed.\n")
+    }
     cat(" \n")
     cat("Convergence criteria: parameters stability=", round(x$ca[1],digits), "\n")
     cat("                    : objective function stability=", round(x$cb,digits), "\n") 
-    cat("                    : relative distance to maximum=", round(x$rdm,digits), "\n")
-    cat(" \n")
-    cat("Goodness-of-fit statistics:", "\n")
-    cat("      optimized objective function:", round(x$fn.value,digits)," \n")
-    cat(" \n")
-    cat(" \n")
-    cat(" \n")
+    if (x$ier == -1){
+        cat("                    : Matrix inversion for RDM failed \n")	
+    }else{
+        cat("                    : Matrix inversion for RDM successful \n")
+    }
+    cat("                    : relative distance to maximum(RDM)=", round(x$rdm,digits), "\n")
+    if(x$istop!=4&x$istop!=5) {
+        cat(" \n")
+        cat("Final parameter values:", "\n")
+        id <- 1:length(x$b)
+        indice <- rep(id*(id+1)/2)
+        se <-sqrt(x$v[indice])
+        wald <- (x$b/se)**2
+        z <- abs(qnorm((1 + .95)/2))
+        binf <- x$b-1.96*se
+        bsup <- x$b+1.96*se
+        
+        tmp <- data.frame("coef"=format(round(x$b,3)),"SE coef"=format(round(se,3)),"Wald"=format(wald,4),"P-value"=round(1 - pchisq(wald, 1),5),"binf"=round(binf,3),"bsup"=round(bsup,3))
+        print(tmp,row.names=F)
+        cat(" \n")
+    }
 }
-
