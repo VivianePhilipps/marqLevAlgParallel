fd1b81ec7ba5f9ba0e73c04c01289c396f9b5487
VivianePhilipps <Viviane.Philipps@u-bordeaux.fr>
2020-03-26 14:48:52 +0100

    dataEx

---
+++ R/dataEx.R
@@ -0,0 +1,14 @@
+#' Simulated dataset
+#' 
+#' The data were simulated ...
+#' 
+#' @name dataEx
+#' @docType data
+#' @format A data frame with 2429 observations on the following 6 variables.
+#' \describe{ \item{i}{subject identification number}
+#' \item{t}{time of measurement} \item{X1}{binary covariate} 
+#' \item{X2}{continous Gaussian standard variable} 
+#' \item{X3}{continous Gaussian variable} 
+#' \item{Y}{longitudinal outcome}}
+#' @keywords datasets
+NULL
