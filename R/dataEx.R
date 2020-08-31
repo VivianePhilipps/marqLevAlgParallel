15377f775f75f398e5393a84ad84c1c61b0afc69
Viviane Philipps <Viviane.Philipps@u-bordeaux.fr>
2020-08-31 16:42:36 +0200

    update documentation

--- R/dataEx.R
+++ R/dataEx.R
@@ -2,7 +2,7 @@
 #' 
 #' Sample of 500 subjects simulated according to a linear mixed model. 
 #' The fixed part of the model included an intercept, 2 natural cubic splines on time 
-#' and their interactions with time-independent covariates $X1$, $X2$ and $X3$. 
+#' and their interactions with time-independent covariates X1, X2 and X3. 
 #' A random intercept and a independant error term were also inclued. 
 #' 
 #' @name dataEx
